a0b94deed373b74267ae821d1faac5fbf4081f5c
inganathi mancam <inganathimancam@gmail.com>
2026-06-13 23:35:21 +0200

    feat: add cybersecurity quiz manager

---
+++ CybersecurityAwerenessBot/QuizManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace CybersecurityAwarenessBot
+{
+    public class QuizManager
+    {
+        public List<QuizQuestion> Questions { get; set; }
+
+        public QuizManager()
+        {
+            Questions = new List<QuizQuestion>
+            {
+                new QuizQuestion
+                {
+                    Question =
+                    "What is phishing?",
+
+                    Options =
+                    new List<string>
+                    {
+                        "A scam email",
+                        "A game",
+                        "An antivirus",
+                        "A password"
+                    },
+
+                    CorrectAnswer =
+                    "A scam email",
+
+                    Explanation =
+                    "Phishing tricks users into giving information."
+                },
+
+                new QuizQuestion
+                {
+                    Question =
+                    "Should you share passwords?",
+
+                    Options =
+                    new List<string>
+                    {
+                        "Yes",
+                        "No"
+                    },
+
+                    CorrectAnswer =
+                    "No",
+
+                    Explanation =
+                    "Passwords should remain private."
+                }
+            };
+        }
+    }
+}
