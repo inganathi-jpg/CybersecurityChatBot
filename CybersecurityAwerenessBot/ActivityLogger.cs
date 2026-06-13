d90702b0d2570a52ef8352f7455baea7446c5edb
inganathi mancam <inganathimancam@gmail.com>
2026-06-13 23:28:54 +0200

    feat: add activity logger

---
+++ CybersecurityAwerenessBot/ActivityLogger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CybersecurityAwarenessBot
+{
+    public class ActivityLogger
+    {
+        private readonly List<string> log = new();
+
+        public void Log(string action)
+        {
+            log.Add($"[{DateTime.Now:HH:mm}] {action}");
+        }
+
+        public string GetRecentLog()
+        {
+            return string.Join("\n",
+                log.TakeLast(10));
+        }
+
+        public string GetFullLog()
+        {
+            return string.Join("\n", log);
+        }
+
+        public int Count()
+        {
+            return log.Count;
+        }
+    }
+}
