b6c3f0bda091c61060af1add485256c3ef875441
inganathi mancam <inganathimancam@gmail.com>
2026-06-13 23:33:02 +0200

    feat: add task manager CRUD operations

---
+++ CybersecurityAwerenessBot/TaskManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CybersecurityAwarenessBot
+{
+    public class TaskManager
+    {
+        private readonly TaskStorageHelper storage =
+            new();
+
+        public void AddTask(
+            string title,
+            string description,
+            string reminder)
+        {
+            List<CyberTask> tasks =
+                storage.LoadTasks();
+
+            int id =
+                tasks.Count == 0
+                ? 1
+                : tasks.Max(x => x.Id) + 1;
+
+            tasks.Add(new CyberTask
+            {
+                Id = id,
+                Title = title,
+                Description = description,
+                Reminder = reminder,
+                CreatedAt =
+                    DateTime.Now.ToString(),
+                IsComplete = false
+            });
+
+            storage.SaveTasks(tasks);
+        }
+
+        public List<CyberTask> GetTasks()
+        {
+            return storage.LoadTasks();
+        }
+
+        public void DeleteTask(int id)
+        {
+            var tasks = storage.LoadTasks();
+
+            tasks.RemoveAll(x => x.Id == id);
+
+            storage.SaveTasks(tasks);
+        }
+
+        public void CompleteTask(int id)
+        {
+            var tasks = storage.LoadTasks();
+
+            var task =
+                tasks.FirstOrDefault(x => x.Id == id);
+
+            if (task != null)
+            {
+                task.IsComplete = true;
+                storage.SaveTasks(tasks);
+            }
+        }
+    }
+}
